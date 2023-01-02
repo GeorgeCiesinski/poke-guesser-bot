f4cc14612cb6a45ab5ced073d8ba3ec30b1c273a
Wissididom <webmaster@wissididom.me>
2023-01-02 20:21:09 +0100

    First steps making sure languages are loaded from file

--- delay.ts
+++ delay.ts
@@ -62,7 +62,7 @@ export default class Delay {
                 }
                 break;
             default:
-                await Util.editReply(interaction, 'Invalid Subcommand', `You used an invalid /delay subcommand (${subcommand}`, lang);
+                await Util.editReply(interaction, lang.obj['error_invalid_subcommand_title'], lang.obj['error_invalid_subcommand_description'].replace('<commandName>', interaction.commandName).replace('<subcommandName>', subcommand), lang);
         }
         // returnEmbed(title, message, image=null)
     }
--- timeout.ts
+++ timeout.ts
@@ -22,7 +22,6 @@ export default class Timeout {
         }
         await interaction.deferReply({ ephemeral: true }); // PokeBot is thinking
         const lang = await Language.getLanguage(interaction.guildId, db);
-        const subcommandgroup = interaction.options.getSubcommandGroup();
         const subcommand = interaction.options.getSubcommand();
         switch (subcommand) {
             case 'set':
@@ -83,9 +82,11 @@ export default class Timeout {
                     };
                 }
                 break;
+            default:
+                await Util.editReply(interaction, lang.obj['error_invalid_subcommand_title'], lang.obj['error_invalid_subcommand_description'].replace('<commandName>', interaction.commandName).replace('<subcommandName>', subcommand), lang);
         }
         // returnEmbed(title, message, image=null)
-        return {title: '', description: ''}; // Should never come here
+        return {title: 'Error', description: 'timeout.ts -> Should never happen'};
     }
 
     private static async setTimeout(serverId: string, userId: string, days: number = 0, hours: number = 0, minutes: number = 0, seconds: number = 0) {
