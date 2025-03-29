63f3d25e149a4436e7ef8ea598cb0f26b0d3a739
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-03-29 13:57:31 +0000

    Update discord.js and migrate from sqlite3 to better-sqlite3 (#125)

--- database.js
+++ database.js
@@ -1,48 +1,48 @@
-import sqlite3 from "sqlite3";
+import { default as DatabaseImpl } from "better-sqlite3";
 
 export default class Database {
   #db = null;
 
   async initDb() {
     this.#db = await new Promise((resolve, reject) => {
-      let db = new sqlite3.Database("./database.db", (err) => {
-        if (err) {
-          console.error(err.message);
-          reject(err);
-          return;
-        }
+      try {
+        let db = new DatabaseImpl("./database.db");
         console.log("Successfully connected to SQLite");
         resolve(db);
-      });
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
     });
     await new Promise((resolve, reject) => {
-      this.#db.run(
-        `CREATE TABLE IF NOT EXISTS kv (key TEXT PRIMARY KEY, value TEXT)`,
-        [],
-        (err) => {
-          if (err) {
-            console.error(err.message);
-            reject(err);
-            return;
-          }
-          console.log(`Successfully made sure table 'kv' exists`);
-          resolve();
-        },
-      );
+      try {
+        const stmt = this.#db.prepare(
+          "CREATE TABLE IF NOT EXISTS kv (key TEXT PRIMARY KEY, value TEXT)",
+        );
+        stmt.run(); // Do not care about the changes made as long as it is successful
+        console.log(`Successfully made sure table 'kv' exists`);
+        resolve();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
     });
   }
 
   async get(key) {
     let rows = await new Promise((resolve, reject) => {
-      this.#db.all(`SELECT * FROM kv WHERE key = ?`, [key], (err, rows) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+      try {
+        const stmt = this.#db.prepare("SELECT * FROM kv WHERE key = ?");
+        const rows = stmt.all(key);
         resolve(rows);
-      });
+      } catch (err) {
+        reject(err);
+      }
     });
     if (rows.length > 0) {
+      if (rows.length > 1) {
+        console.warn(`[GET] Warning: more than 1 row found for key ${key}`);
+      }
       return rows[0].value;
     } else {
       return "";
@@ -51,58 +51,59 @@ export default class Database {
 
   async set(key, value) {
     await new Promise((resolve, reject) => {
-      this.#db.run(
-        `INSERT INTO kv (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = excluded.value`,
-        [key, value],
-        (err) => {
-          if (err) {
-            console.error(err.message);
-            reject(err);
-            return;
-          }
-          console.log(
-            `Successfully upserted value '${key}'='${JSON.stringify(value)}'`,
-          );
-          resolve();
-        },
-      );
+      try {
+        const stmt = this.#db.prepare(
+          "INSERT INTO kv (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = excluded.value",
+        );
+        stmt.run(key, value); // Do not care about the changes made as long as it is successful
+        console.log(
+          `Successfully upserted value '${key}'='${JSON.stringify(value)}'`,
+        );
+        resolve();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
     });
   }
 
   async delete(key) {
     let rows = await new Promise((resolve, reject) => {
-      this.#db.all(`SELECT * FROM kv WHERE key = ?`, [key], (err, rows) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+      try {
+        const stmt = this.#db.prepare("SELECT * FROM kv WHERE key = ?");
+        const rows = stmt.all(key);
         resolve(rows);
-      });
+      } catch (err) {
+        reject(err);
+      }
     });
     if (rows.length > 0) {
+      if (rows.length > 1) {
+        console.warn(`[DELETE] Warning: more than 1 row found for key ${key}`);
+      }
       await new Promise((resolve, reject) => {
-        this.#db.run(`DELETE FROM kv WHERE key = ?`, [key], (err) => {
-          if (err) {
-            console.error(err.message);
-            reject(err);
-            return;
-          }
+        try {
+          const stmt = this.#db.prepare("DELETE FROM kv WHERE key = ?");
+          stmt.run(key); // Do not care about the changes made as long as it is successful
           console.log(`Successfully deleted '${key}'`);
           resolve();
-        });
+        } catch (err) {
+          console.error(err);
+          reject(err);
+        }
       });
     }
   }
 
   async list() {
     let rows = await new Promise((resolve, reject) => {
-      this.#db.all(`SELECT * FROM kv`, [], (err, rows) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+      try {
+        const stmt = this.#db.prepare("SELECT * FROM kv");
+        const rows = stmt.all();
         resolve(rows);
-      });
+      } catch (err) {
+        reject(err);
+      }
     });
     if (rows.length > 0) {
       return rows.map((row) => row.key).join("\n");
