c3c1f57d20e4e8f737234868ee89543d43600c10
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-10-30 18:13:35 +0100

    Simplify mod check in reveal.ts

--- src/reveal.ts
+++ src/reveal.ts
@@ -32,69 +32,65 @@ export default class Reveal {
         }
       }
     }
-    if (isMod) {
-      console.log("isMod: true");
-      const encounter = await db.getEncounter(
-        interaction.guildId!,
-        interaction.channelId,
+    if (!isMod) {
+      await Util.editReply(
+        interaction,
+        lang.obj["reveal_no_encounter_title"],
+        lang.obj["reveal_no_encounter_description"],
+        lang,
       );
-      if (encounter.length > 0) {
-        const pokemonNames: string[] = [];
-        let englishIndex = 0;
-        for (let i = 0; i < encounter.length; i++) {
-          // console.log(encounter)
-          encounter[i].name = encounter[i].getDataValue("name");
-          encounter[i].language = encounter[i].getDataValue("language");
-          if (!encounter[i].name) continue;
-          const lowercaseName = encounter[i].name?.toLowerCase();
-          if (!pokemonNames.includes(lowercaseName ?? "")) {
-            pokemonNames.push(lowercaseName ?? "");
-          }
-          if (encounter[i].language === "en") englishIndex = i;
+      return;
+    }
+    const encounter = await db.getEncounter(
+      interaction.guildId!,
+      interaction.channelId,
+    );
+    if (encounter.length > 0) {
+      const pokemonNames: string[] = [];
+      let englishIndex = 0;
+      for (let i = 0; i < encounter.length; i++) {
+        // console.log(encounter)
+        encounter[i].name = encounter[i].getDataValue("name");
+        encounter[i].language = encounter[i].getDataValue("language");
+        if (!encounter[i].name) continue;
+        const lowercaseName = encounter[i].name?.toLowerCase();
+        if (!pokemonNames.includes(lowercaseName ?? "")) {
+          pokemonNames.push(lowercaseName ?? "");
         }
-        // build string to put in between brackets
-        let inBrackets = "";
-        for (let i = 0; i < pokemonNames.length; i++) {
-          if (i == englishIndex) continue;
-          if (inBrackets == "") inBrackets = Util.capitalize(pokemonNames[i]);
-          else inBrackets += `, ${Util.capitalize(pokemonNames[i])}`;
-        }
-        console.log(
-          `Mod requested reveal: ${
-            encounter[englishIndex].name
-          } (${inBrackets})`,
-        );
-        // returnEmbed(title, message, image=null)
-        await Util.editReply(
-          interaction,
-          lang.obj["reveal_pokemon_escaped_title"],
-          lang.obj["reveal_pokemon_escaped_description"]
-            .replace(
-              "<englishPokemon>",
-              encounter[englishIndex].name
-                ? Util.capitalize(encounter[englishIndex].name!)
-                : "N/A",
-            )
-            .replace("<inBrackets>", inBrackets),
-          lang,
-        );
-        await db.clearEncounters(interaction.guildId!, interaction.channelId);
-        await Lightning.checkLightning(interaction, db);
-      } else {
-        // returnEmbed(title, message, image=null)
-        await Util.editReply(
-          interaction,
-          lang.obj["reveal_no_encounter_title"],
-          lang.obj["reveal_no_encounter_description"],
-          lang,
-        );
+        if (encounter[i].language === "en") englishIndex = i;
       }
+      // build string to put in between brackets
+      let inBrackets = "";
+      for (let i = 0; i < pokemonNames.length; i++) {
+        if (i == englishIndex) continue;
+        if (inBrackets == "") inBrackets = Util.capitalize(pokemonNames[i]);
+        else inBrackets += `, ${Util.capitalize(pokemonNames[i])}`;
+      }
+      console.log(
+        `Mod requested reveal: ${encounter[englishIndex].name} (${inBrackets})`,
+      );
+      // returnEmbed(title, message, image=null)
+      await Util.editReply(
+        interaction,
+        lang.obj["reveal_pokemon_escaped_title"],
+        lang.obj["reveal_pokemon_escaped_description"]
+          .replace(
+            "<englishPokemon>",
+            encounter[englishIndex].name
+              ? Util.capitalize(encounter[englishIndex].name!)
+              : "N/A",
+          )
+          .replace("<inBrackets>", inBrackets),
+        lang,
+      );
+      await db.clearEncounters(interaction.guildId!, interaction.channelId);
+      await Lightning.checkLightning(interaction, db);
     } else {
       // returnEmbed(title, message, image=null)
       await Util.editReply(
         interaction,
-        lang.obj["reveal_no_mod_title"],
-        lang.obj["reveal_no_mod_description"],
+        lang.obj["reveal_no_encounter_title"],
+        lang.obj["reveal_no_encounter_description"],
         lang,
       );
     }
